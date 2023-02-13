33c4caa734eb0970e5d1fa6628f7677e8f792e33
Grovre <50428844+Grovre@users.noreply.github.com>
2023-02-13 17:11:30 -0600

    Documentation

--- LightLog/Impl/AsyncLogger.cs
+++ LightLog/Impl/AsyncLogger.cs
@@ -3,6 +3,12 @@ using LightLog.Interfaces;
 
 namespace LightLog.Impl;
 
+/// <summary>
+/// A logger class that runs asynchronously on its own thread.
+/// Logging only passes the string and everything else will be
+/// done on the logger thread to avoid the loss of time in
+/// string interpolation and printing. Order is not guaranteed.
+/// </summary>
 public sealed class AsyncLogger : ILogger
 {
     internal readonly BlockingCollection<string> BlockingQueue;
@@ -10,6 +16,10 @@ public sealed class AsyncLogger : ILogger
     private readonly CancellationTokenSource _cancellationToken;
     public TextWriter _textWriter { get; set; }
 
+    /// <summary>
+    /// Instantiates an AsyncLogger
+    /// </summary>
+    /// <param name="textWriter">TextWriter to write to</param>
     public AsyncLogger(TextWriter textWriter)
     {
         BlockingQueue = new();
@@ -42,12 +52,27 @@ public sealed class AsyncLogger : ILogger
         }).Start();
     }
 
+    /// <summary>
+    /// Forces the logging thread to stop allowing new
+    /// logs to be added, stop writing, and exits the thread
+    /// peacefully. Any threads that have called
+    /// <code>WaitForFinishedLoggingEvent</code>
+    /// will stop waiting and continue.
+    /// </summary>
     public void ForceStopLogging()
     {
         _cancellationToken.Cancel();
         BlockingQueue.CompleteAdding();
+        CompleteLogging();
     }
 
+    /// <summary>
+    /// Adds the string to the logs that are
+    /// being printed. Everything that happens
+    /// in this method happens asynchronously.
+    /// </summary>
+    /// <param name="log">The string to print, prefixed by the time of logging</param>
+    /// <returns>Whether the log was added or not. Will always be true unless this logger has completed logging.</returns>
     public bool Log(string log)
     {
         if (BlockingQueue.IsAddingCompleted)
@@ -56,16 +81,34 @@ public sealed class AsyncLogger : ILogger
         return true;
     }
 
+    /// <summary>
+    /// Denies any more logs to be logged by this
+    /// logger. The logger will finish printing the
+    /// remaining logs until disposed of, or with
+    /// the guarantee of finishing if
+    /// <code>WaitForFinishedLoggingEvent</code>
+    /// was called.
+    /// </summary>
     public void CompleteLogging()
     {
         BlockingQueue.CompleteAdding();
     }
 
+    /// <summary>
+    /// Blocks the calling thread until the
+    /// logger has completed and is empty,
+    /// or forced stop.
+    /// </summary>
     public void WaitForFinishedLoggingEvent()
     {
         DoneLoggingEvent.WaitOne();
     }
     
+    /// <summary>
+    /// Disposes of everything this object
+    /// uses internally, along with the TextWriter
+    /// used to write to.
+    /// </summary>
     public void Dispose()
     {
         
--- LightLog/Impl/Logger.cs
+++ LightLog/Impl/Logger.cs
@@ -2,27 +2,55 @@
 
 namespace LightLog.Impl;
 
+/// <summary>
+/// A synchronous logger that writes to the given
+/// TextWriter, prefixed by the local date and time.
+/// </summary>
 public sealed class Logger : ILogger, IRedirection<TextWriter>
 {
     public TextWriter _textWriter { get; set; }
 
+    /// <summary>
+    /// Creates a new Logger for logging
+    /// </summary>
+    /// <param name="textWriter">The TextWriter to log to</param>
     public Logger(TextWriter textWriter)
     {
         _textWriter = textWriter;
     }
 
+    /// <summary>
+    /// Changes the TextWriter used and returns
+    /// the previous one in use. Dispose the
+    /// returned TextWriter if it is no longer
+    /// going to be used.
+    /// </summary>
+    /// <param name="textWriter"></param>
+    /// <returns></returns>
     public TextWriter Redirect(TextWriter textWriter)
     {
         (_textWriter, textWriter) = (textWriter, _textWriter);
         return textWriter;
     }
 
+    /// <summary>
+    /// Writes a log to the TextWriter that is
+    /// prefixed by DateTime.Now
+    /// </summary>
+    /// <param name="log">The log to write</param>
+    /// <returns>Whether the log was written correctly. Always true.</returns>
     public bool Log(string log)
     {
         _textWriter.WriteLine($"{ILogger.DatePrefix} {log}");
         return true;
     }
 
+    /// <summary>
+    /// Disposes of everything this object
+    /// uses internally, along with the TextWriter
+    /// used to write to.
+    /// </summary>
+    /// </summary>
     public void Dispose()
     {
         _textWriter.Dispose();
