ebef4ec7a01cd42dcf0bc0f87bccdcde2fef470d
Grovre <50428844+Grovre@users.noreply.github.com>
2023-05-21 12:05:55 -0500

    Shortened global logger code

--- LightLog/Impl/GlobalLogger.cs
+++ LightLog/Impl/GlobalLogger.cs
@@ -4,23 +4,11 @@ namespace LightLog.Impl;
 
 public partial class Logger
 {
-    private static ILogger? _globalLogger;
-
-    /// <summary>
-    /// A static and globally-shared logger for simple use cases
-    /// </summary>
-    public static ILogger GlobalLogger
+    private static Logger? _logger;
+    
+    public static Logger Shared
     {
-        get
-        {
-            if (_globalLogger == null)
-            {
-                _globalLogger = new Logger(Console.Out);
-            }
-
-            return _globalLogger;
-        }
-
-        set => _globalLogger = value;
+        get => _logger ??= new Logger(Console.Out);
+        set => _logger = value;
     }
 }
